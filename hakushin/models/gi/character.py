e678191a0bf5a313976ee3dbf8515615126a556d
seria <seria.ati@gmail.com>
2024-06-02 07:09:54 +0900

    feat: Add icon attr to CharacterConstellation

--- hakushin/models/gi/character.py
+++ hakushin/models/gi/character.py
@@ -91,6 +91,11 @@ class CharacterConstellation(APIModel):
     name: str = Field(alias="Name")
     description: str = Field(alias="Desc")
     parameters: list[float] = Field(alias="ParamList")
+    icon: str = Field(alias="Icon")
+
+    @field_validator("icon", mode="before")
+    def _convert_icon(cls, value: str) -> str:
+        return f"https://api.hakush.in/gi/UI/{value}.webp"
 
 
 class UpgradeMaterial(APIModel):
