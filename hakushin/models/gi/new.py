998de19d6638ad4ea81fd7728ee5b3fb0e106eb1
seria <seria.ati@gmail.com>
2024-11-21 12:51:35 +0800

    fix!: Change type of gi.New.character_ids to list[str | int]

--- hakushin/models/gi/new.py
+++ hakushin/models/gi/new.py
@@ -10,7 +10,7 @@ __all__ = ("New",)
 class New(APIModel):
     """Genshin Impact new stuff."""
 
-    character_ids: list[int] = Field(alias="character")
+    character_ids: list[str | int] = Field(alias="character")
     weapon_ids: list[int] = Field(alias="weapon")
     artifact_set_ids: list[int] = Field(alias="artifact")
     monster_ids: list[int] = Field(alias="monster")
