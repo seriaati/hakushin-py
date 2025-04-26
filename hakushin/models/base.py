676c9e7a7c86170a827c91749d077f712380f93d
seria <seria.ati@gmail.com>
2025-04-26 17:43:19 +0900

    fix: Remove deprecated use of model_fields

--- hakushin/models/base.py
+++ hakushin/models/base.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Self
+from typing import Self
 
 from pydantic import BaseModel, model_validator
 
@@ -10,17 +10,9 @@ from ..utils import cleanup_text, remove_ruby_tags, replace_device_params
 class APIModel(BaseModel):
     """Base class for all models in hakushin-py."""
 
-    @property
-    def fields(self) -> dict[str, Any]:
-        """Return all fields of the model as a dictionary."""
-        schema = self.model_fields
-        field_names = schema.keys()
-        field_values = {name: getattr(self, name) for name in field_names}
-        return field_values
-
     @model_validator(mode="after")
     def _format_fields(self) -> Self:
-        for field_name, field_value in self.fields.items():
+        for field_name, field_value in self.model_dump().items():
             if field_name in {"name", "description", "story"} and isinstance(field_value, str):
                 setattr(
                     self,
